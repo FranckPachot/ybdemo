7742a70b55dc6b121c5c4cfee2728d1084ce34a0
Franck Pachot (YB) <33070466+FranckPachot@users.noreply.github.com>
2022-01-10 12:50:07 +0100

    Update demo-inserts.sh

--- examples/demo-inserts.sh
+++ examples/demo-inserts.sh
@@ -1,4 +1,6 @@
 (cd yb-rf3 && docker-compose up -d)
+
+docker exec -i yb-tserver-1 bash <<'EXEC'
 curl -sL https://github.com/FranckPachot/ybdemo/releases/download/v0.0.1/YBDemo-0.0.1-SNAPSHOT-jar-with-dependencies.jar > YBDemo.jar
 cat > hikari.properties <<CAT
 dataSourceClassName=com.yugabyte.ysql.YBClusterAwareDataSource
@@ -9,12 +11,13 @@ maximumPoolSize=6
 maxLifetime=300000
 idleTimeout=120000
 autoCommit=true
+connectionInitSql=create table if not exists ybdemo(id bigint generated always as identity, ts timestamptz default now(), pid int default pg_backend_pid(), host text default host(inet_server_addr()), message text); prepare insert(text) as insert into ybdemo(message) values ('\$1') returning format('%20s %s %s %s %s','',ts,pid,host,message);
+autoCommit=true
 CAT
 java -jar YBDemo.jar <<'SQL'
-drop table ybdemo; pg_sleep(5000);
-create table ybdemo(id bigint generated always as identity, ts timestamptz default now(), pid int default pg_backend_pid(), host text default host(inet_server_addr()), message text); pg_sleep(5000);
-insert into ybdemo(message) values ('hello') returning format('%20s %s %s %s %s','',ts,pid,host,message);
-insert into ybdemo(message) values ('hola') returning format('%20s %s %s %s %s','',ts,pid,host,message);
-insert into ybdemo(message) values ('bonjour') returning format('%20s %s %s %s %s','',ts,pid,host,message);
+execute insert('hello');
+execute insert('hola');
+execute insert('bonjour');
 select format('%12s rows/s',to_char(count(*)/extract(epoch from max(ts)-min(ts)),'99999999.99')) from ybdemo;
 SQL
+EXEC

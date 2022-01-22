e1d2e4097c54985bb2c7ddf88725ea2ef48ef634
Franck Pachot <franck.pachot@dbi-services.com>
2022-01-22 22:40:27 +0100

    add retries

--- docker/yb-lab/gen-yb-docker-compose.sh
+++ docker/yb-lab/gen-yb-docker-compose.sh
@@ -48,6 +48,8 @@ services:
       command: ["bash","client/ybdemo.sh","run"]
       deploy:
           replicas: 3
+      restart_policy:
+        condition: on-failure
       depends_on:
       - yb-tserver-0
 

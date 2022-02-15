7a5751b13c175e3e6c4c1da74b6df2d1c6dbb409
Franck Pachot (YB) <33070466+FranckPachot@users.noreply.github.com>
2022-02-15 16:46:50 +0100

    Update ybwr.sql

--- docker/yb-lab/client/ybwr.sql
+++ docker/yb-lab/client/ybwr.sql
@@ -1,16 +1,19 @@
+# the "ybwr_snapshots" table stores snapshots of tserver metrics, gathered by "ybwr_snap", reading all endpoints known by "yb_servers()"
 create table ybwr_snapshots(host text default '', ts timestamptz default now(),  metrics jsonb, primary key (host,ts));
 create or replace function ybwr_snap() returns timestamptz as $DO$ declare i record; begin for i in (select host from yb_servers()) loop execute format($COPY$copy ybwr_snapshots(host,metrics) from program $BASH$exec 5<>/dev/tcp/%s/9000 ; awk 'BEGIN{printf "%s\t"}/[[]/{p=1}p==1{printf $0}' <&5 & printf "GET /metrics HTTP/1.0\r\n\r\n" >&5$BASH$ $COPY$,i.host,i.host); end loop; return now(); end; $DO$ language plpgsql;
+# most metrics are cumulative and the "ybwr_report" view show the delta between two snapshots
 create or replace view ybwr_report as
-select value,round(value/seconds) rate,host,tablet_id,table_name,metric_name,ts,relative_snap_id
+select value,round(value/seconds) rate,host,tablet_id,namespace_name,table_name,metric_name,ts,relative_snap_id
 from (
-select ts,host,metric_name,table_name,tablet_id
-,metric_value-lead(metric_value)over(partition by host,table_name,tablet_id,metric_name order by ts desc) as value
-,extract(epoch from ts-lead(ts)over(partition by host,table_name,tablet_id,metric_name order by ts desc)) as seconds
-,rank()over(partition by host,table_name,tablet_id,metric_name order by ts desc) as relative_snap_id
+select ts,host,metric_name,namespace_name,table_name,tablet_id
+,metric_value-lead(metric_value)over(partition by host,namespace_name,table_name,tablet_id,metric_name order by ts desc) as value
+,extract(epoch from ts-lead(ts)over(partition by host,namespace_name,table_name,tablet_id,metric_name order by ts desc)) as seconds
+,rank()over(partition by host,namespace_name,table_name,tablet_id,metric_name order by ts desc) as relative_snap_id
 from (
 select host,ts
  ,jsonb_array_elements(metrics)->>'type' as type
  ,jsonb_array_elements(metrics)->>'id'   as tablet_id
+ ,jsonb_array_elements(metrics)->'attributes'->>'namespace_name'  as namespace_name
  ,jsonb_array_elements(metrics)->'attributes'->>'table_name'  as table_name
  ,jsonb_array_elements(jsonb_array_elements(metrics)->'metrics')->>'name' as metric_name
  ,(jsonb_array_elements(jsonb_array_elements(metrics)->'metrics')->>'value')::bigint as metric_value
@@ -19,7 +22,15 @@ select host,ts
 from ybwr_snapshots
 ) tablets
 ) tablets_delta where value>0;
+# a convenient "ybwr_last" shows the last snapshot
 create or replace view ybwr_last as select * from ybwr_report where relative_snap_id=1;
+# this purges to the snaps to keep only the last 100 snapshots
+delete from ybwr_snapshots where ts not in (select distinct ts from ybwr_snapshots order by ts desc limit 100);
+# example:
 select ybwr_snap();
 select ybwr_snap();
-select value,rate,host,tablet_id,table_name,metric_name from ybwr_last order by ts desc,value desc,host,metric_name,table_name,tablet_id;
+select value,rate,namespace_name,table_name,metric_name,host,tablet_id
+,to_char(100*value/sum(value)over(partition by namespace_name,table_name,metric_name),'999%') as "%table"
+,sum(value)over(partition by namespace_name,table_name,metric_name) as "table"
+from ybwr_last where table_name='write_read_test'
+order by ts desc,"table" desc,value desc,host,metric_name,table_name,tablet_id;

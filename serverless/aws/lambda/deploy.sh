91c7a71de350857d6f3c1a809eea9212355522b5
Franck Pachot <C@pachot.net>
2022-03-14 23:00:00 +0000

    ok with pool

--- serverless/aws/lambda/deploy.sh
+++ serverless/aws/lambda/deploy.sh
@@ -22,25 +22,30 @@ CAT
 
 cat > ybdemo-lambda.js <<'CAT'
 
-exports.handler = async(event) => {
-    const {Client} = require('pg');
-    //const body = JSON.parse(event.body);
-    //const query = {
-        //text: "insert into test (text) values($1)",
-        //values: [body.text],
-    //};
-    const client = new Client({
+const { Pool } = require('pg')
+const db = new Pool({
         user: 'yugabyte',
         host: 'a0b821173a97b49ef90c6dd7d9b26551-79341159.eu-west-1.elb.amazonaws.com',
         database: 'yugabyte',
         password: '',
         port: 5433,
-    });
-    client.connect();
-    const result = await client.query("select * from yb_servers()");
-    const resultString = JSON.stringify(result.rows[0]);    
+  max: 5,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 2000,
+})
 
-    client.end();
+exports.handler = async(event) => {
+  var resultString="."
+    db.connect();
+    //const body = JSON.parse(event.body);
+    //const query = {
+        //text: "insert into test (text) values($1)",
+        //values: [body.text],
+    //};
+    const result = await db.query("select pg_backend_pid()");
+    resultString = JSON.stringify(result.rows[0]);    
+
+    //client.release();
 
     const response = {
         "statusCode":200,
@@ -69,7 +74,9 @@ rm ybdemo-lambda-role.zip ; zip -r ybdemo-lambda-role.zip ybdemo-lambda.js node_
     --role $( aws iam get-role --role-name ybdemo-lambda-role --output json | jq -r '."Role"."Arn"' )
 sleep 5
 
+for i in {1..5} ; do
 aws lambda invoke --function-name ybdemo-lambda --cli-binary-format raw-in-base64-out --payload '{"numberA":"21","numberB":"21"}' .tmp.js && jq . < .tmp.js
+done
 
 aws lambda delete-function --function-name ybdemo-lambda
 }

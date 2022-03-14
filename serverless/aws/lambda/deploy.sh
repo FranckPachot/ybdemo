ebd386b22a69498649c17fbbfac4f17af98d54a7
Franck Pachot <C@pachot.net>
2022-03-14 22:36:56 +0000

    ok to connect and query

--- serverless/aws/lambda/deploy.sh
+++ serverless/aws/lambda/deploy.sh
@@ -1,4 +1,9 @@
 
+# while sleep 1 ; do [ deploy.sh -nt .tmp.js ] && sh deploy.sh ; done
+ 
+
+export AWS_PAGER="" 
+
 cat > trust-policy.json <<'CAT'
 {
   "Version": "2012-10-17",
@@ -16,21 +21,47 @@ CAT
 
 
 cat > ybdemo-lambda.js <<'CAT'
-exports.handler = async function(event) {
-  const { numberA, numberB} = event;
 
-  return {
-    "sumResult": numberA + numberB
-  };
-}
+exports.handler = async(event) => {
+    const {Client} = require('pg');
+    //const body = JSON.parse(event.body);
+    //const query = {
+        //text: "insert into test (text) values($1)",
+        //values: [body.text],
+    //};
+    const client = new Client({
+        user: 'yugabyte',
+        host: 'a0b821173a97b49ef90c6dd7d9b26551-79341159.eu-west-1.elb.amazonaws.com',
+        database: 'yugabyte',
+        password: '',
+        port: 5433,
+    });
+    client.connect();
+    const result = await client.query("select * from yb_servers()");
+    const resultString = JSON.stringify(result.rows[0]);    
+
+    client.end();
+
+    const response = {
+        "statusCode":200,
+        "body":resultString
+    };
+
+    return response;
+
+};
+
 CAT
 
-zip ybdemo-lambda-role.zip ybdemo-lambda.js
 
+echo "$*" | grep role && {
 aws iam create-role --role-name ybdemo-lambda-role --assume-role-policy-document file://trust-policy.json
 sleep 1
+}
 
-aws lambda create-function \
+npm install pg
+node ybdemo-lambda.js && {
+rm ybdemo-lambda-role.zip ; zip -r ybdemo-lambda-role.zip ybdemo-lambda.js node_modules ; aws lambda create-function \
     --function-name ybdemo-lambda \
     --runtime nodejs14.x \
     --zip-file fileb://ybdemo-lambda-role.zip \
@@ -39,7 +70,10 @@ aws lambda create-function \
 sleep 5
 
 aws lambda invoke --function-name ybdemo-lambda --cli-binary-format raw-in-base64-out --payload '{"numberA":"21","numberB":"21"}' .tmp.js && jq . < .tmp.js
-sleep 1
 
 aws lambda delete-function --function-name ybdemo-lambda
+}
+
+echo "$*" | grep role && {
 aws iam delete-role --role-name ybdemo-lambda-role
+}

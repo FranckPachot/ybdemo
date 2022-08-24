a3558e0c9e4d4d3b5056482d7e0ddf1457e874a4
Franck Pachot <mail@pachot.net>
2022-08-24 12:30:26 +0200

    added duplicate index and yb local table

--- docker/yb-lab/client/yftt-tablespaces.sql
+++ docker/yb-lab/client/yftt-tablespaces.sql
@@ -2,7 +2,7 @@
 ssh -L 5433:localhost:5433 -L 7000:localhost:7000 root@docker
 [ -f ybdemo ] || git clone git@github.com:FranckPachot/ybdemo.git
 cd ybdemo/docker/yb-lab 
-sh gen-yb-docker-compose.sh geo 3
+sh gen-yb-docker-compose.sh geo
 
 psql -h localhost -p 5433 -d yugabyte -U yugabyte 
 --\i client/yftt-tablespaces.sql
@@ -61,12 +61,17 @@ create tablespace "us-west" with ( replica_placement= $$
 } $$) ;
 
 create table demo_gdpr ( 
- user_country char(2) primary key,
-  user_uuid uuid default gen_random_uuid()
+ user_country char(2),
+  user_uuid uuid default gen_random_uuid(),
+  primary key (user_country, user_uuid)
 ) partition by list(user_country);
 
 create table demo_gdpr_eu partition of demo_gdpr
  for values in ('FR','DE','IT') tablespace "eu-west"
+;
+
+create table demo_gdpr_uk partition of demo_gdpr
+ for values in ('UK') 
 ;
 
 create table demo_gdpr_ch partition of demo_gdpr
@@ -89,6 +94,8 @@ create tablespace "rf3" with ( replica_placement= $$
     ]
 } $$) ;
 
+alter table demo_gdpr_uk set tablespace "rf3";
+
 create tablespace "rf5" with ( replica_placement= $$
 {
     "num_replicas": 5,
@@ -98,6 +105,8 @@ create tablespace "rf5" with ( replica_placement= $$
 { "cloud": "cloud", "region": "us-west", "zone": "az1"   , "min_num_replicas": 1 }
     ]
 } $$) ;
+
+alter table demo_gdpr_ca set tablespace "rf5";
 
 
 ---# we can define the leader placement
@@ -123,8 +132,59 @@ select host, cloud, region, zone from yb_servers() order by 2,3,4;
 \! docker pause yb-tserver-0  
 \! docker pause yb-tserver-9  
 
+\! docker unpause yb-tserver-0  
+\! docker unpause yb-tserver-9  
 
 
+---# duplicate index
+
+alter table demo add column answer int;
+explain select * from demo where id=42;
+create index demo_answer_covering_eu on demo (answer) include (id) tablespace "eu-west";
+create index demo_answer_covering_us on demo (answer)  include (id)tablespace "us-west";
+explain select * from demo where answer=42;
+\! docker exec -it yb-tserver-11 ysqlsh -h yb-tserver-11
+explain select * from demo where answer=42;
+drop index demo_answer_covering_us;
+explain select * from demo where answer=42;
+\q
+
+--  yb_is_local_table
+
+\d+ demo_gdpr;
+insert into demo_gdpr values  ('CA'),('CH'),('FR'),( 'DE'),( 'IT'),('UK') ;
+insert into demo_gdpr values  ('CA'),('CH'),('FR'),( 'DE'),( 'IT'),('UK') ;
+insert into demo_gdpr values  ('CA'),('CH'),('FR'),( 'DE'),( 'IT'),('UK') ;
+insert into demo_gdpr values  ('CA'),('CH'),('FR'),( 'DE'),( 'IT'),('UK') ;
+
+\pset pager off
+explain analyze
+select * from demo_gdpr;
+select * from demo_gdpr where yb_is_local_table(tableoid);
+explain analyze
+select * from demo_gdpr where yb_is_local_table(tableoid);
+
+
+
+
+
+
+
+
+
+
+
+
+\! docker exec -it yb-tserver-12 ysqlsh -h yb-tserver-12
+select * from demo_gdpr where yb_is_local_table(tableoid);
+\q
+
+\! docker exec -it yb-tserver-13 ysqlsh -h yb-tserver-13
+select * from demo_gdpr where yb_is_local_table(tableoid);
+\q
+
+\! docker exec -it yb-tserver-16 ysqlsh -h yb-tserver-16
+select * from demo_gdpr where yb_is_local_table(tableoid);
 
 
 

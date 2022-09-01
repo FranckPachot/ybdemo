3485ae44eef8c4fe9c91ac49b0e1d71f21fceae8
Franck Pachot <mail@pachot.net>
2022-09-01 20:51:08 +0000

    Thu Sep  1 20:51:08 GMT 2022

--- docker/yb-lab/client/yftt-tablespaces.sql
+++ docker/yb-lab/client/yftt-tablespaces.sql
@@ -2,8 +2,10 @@
 ssh -L 5433:localhost:5433 -L 7000:localhost:7000 root@docker
 [ -f ybdemo ] || git clone git@github.com:FranckPachot/ybdemo.git
 cd ybdemo/docker/yb-lab 
+git pull
 sh gen-yb-docker-compose.sh geo
 
+df -h /
 psql -h localhost -p 5433 -d yugabyte -U yugabyte 
 --\i client/yftt-tablespaces.sql
 \pset pager off
@@ -60,30 +62,35 @@ create tablespace "us-west" with ( replica_placement= $$
     ]
 } $$) ;
 
+---# create a partitioned table
 create table demo_gdpr ( 
- user_country char(2),
+  user_country char(2),
   user_uuid uuid default gen_random_uuid(),
+  user_counter int default 0,
   primary key (user_country, user_uuid)
 ) partition by list(user_country);
 
 create table demo_gdpr_eu partition of demo_gdpr
  for values in ('FR','DE','IT') tablespace "eu-west"
+ split into 4 tablets
 ;
 
 create table demo_gdpr_uk partition of demo_gdpr
  for values in ('UK') 
+ split into 4 tablets
 ;
 
 create table demo_gdpr_ch partition of demo_gdpr
  for values in ('CH') tablespace "eu-west"
+ split into 4 tablets
 ;
 
-create table demo_gdpr_ca partition of demo_gdpr
- for values in ('CA') tablespace "us-west"
+create table demo_gdpr_us partition of demo_gdpr
+ for values in ('US') tablespace "us-west"
+ split into 4 tablets
 ;
 
 ---# the replication factor can be changed
-
 create tablespace "rf3" with ( replica_placement= $$
 {
     "num_replicas": 3,
@@ -106,7 +113,7 @@ create tablespace "rf5" with ( replica_placement= $$
     ]
 } $$) ;
 
-alter table demo_gdpr_ca set tablespace "rf5";
+alter table demo_gdpr_us set tablespace "rf5";
 
 
 ---# we can define the leader placement
@@ -115,57 +122,115 @@ create tablespace "rf5-pref-eu" with ( replica_placement= $$
 {
     "num_replicas": 5,
     "placement_blocks": [
-{ "cloud": "cloud", "region": "eu-west", "zone": "az1"   , "min_num_replicas": 1 
+{ "cloud": "cloud", "region": "eu-west", "zone": "az1"   , "min_num_replicas": 2 
   , "leader_preference": 1 },
-{ "cloud": "cloud", "region": "us-east", "zone": "az1"   , "min_num_replicas": 1 
+{ "cloud": "cloud", "region": "us-east", "zone": "az1"   , "min_num_replicas": 2 
   , "leader_preference": 2 },
 { "cloud": "cloud", "region": "us-west", "zone": "az1"   , "min_num_replicas": 1 
   , "leader_preference": 3 }
     ]
 } $$) ;
 
-alter table demo_gdpr_ch set tablespace "rf5-pref-eu";
 
 
 ---# region failure
 select host, cloud, region, zone from yb_servers() order by 2,3,4;
+
 \! docker pause yb-tserver-0  
 \! docker pause yb-tserver-9  
+
+\c - - - 5450
+show listen_addresses ;
+
+with countries(country) as (
+ values ('CH'),('FR'),('DE'),('IT'),('UK'),('US')
+) insert into demo_gdpr(user_country)
+ select country from countries, generate_series(1,1000);
+
+select * from demo_gdpr order by user_uuid;
 
 \! docker unpause yb-tserver-0  
 \! docker unpause yb-tserver-9  
 
-
----# duplicate index
-
-alter table demo add column answer int;
-explain select * from demo where id=42;
-create index demo_answer_covering_eu on demo (answer) include (id) tablespace "eu-west";
-create index demo_answer_covering_us on demo (answer)  include (id)tablespace "us-west";
-explain select * from demo where answer=42;
-\! docker exec -it yb-tserver-11 ysqlsh -h yb-tserver-11
-explain select * from demo where answer=42;
-drop index demo_answer_covering_us;
-explain select * from demo where answer=42;
-\q
-
 --  yb_is_local_table
 
 \d+ demo_gdpr;
-insert into demo_gdpr values  ('CA'),('CH'),('FR'),( 'DE'),( 'IT'),('UK') ;
-insert into demo_gdpr values  ('CA'),('CH'),('FR'),( 'DE'),( 'IT'),('UK') ;
-insert into demo_gdpr values  ('CA'),('CH'),('FR'),( 'DE'),( 'IT'),('UK') ;
-insert into demo_gdpr values  ('CA'),('CH'),('FR'),( 'DE'),( 'IT'),('UK') ;
+insert into demo_gdpr values  ('US'),('CH'),('FR'),( 'DE'),( 'IT'),('UK') ;
+insert into demo_gdpr values  ('US'),('CH'),('FR'),( 'DE'),( 'IT'),('UK') ;
+insert into demo_gdpr values  ('US'),('CH'),('FR'),( 'DE'),( 'IT'),('UK') ;
+insert into demo_gdpr values  ('US'),('CH'),('FR'),( 'DE'),( 'IT'),('UK') ;
 
 \pset pager off
-explain analyze
+explain 
 select * from demo_gdpr;
 select * from demo_gdpr where yb_is_local_table(tableoid);
-explain analyze
+explain 
 select * from demo_gdpr where yb_is_local_table(tableoid);
+show listen_addresses;
+\c - - - 5433
+show listen_addresses;
+select * from demo_gdpr where yb_is_local_table(tableoid) order by 2;
+explain 
+select * from demo_gdpr where yb_is_local_table(tableoid) order by 2;
+
+---# duplicate index
+alter table demo add column answer int;
+insert into demo select n, n from generate_series(1,100) n;
+explain select * from demo where answer=42;
+create index demo_answer_covering_eu on demo (answer) include (id) tablespace "eu-west";
+create index demo_answer_covering_us on demo (answer) include (id) tablespace "us-west";
+explain select * from demo where answer=42;
+\c - - - 5450
+show listen_addresses;
+explain select * from demo where answer=42;
+drop index demo_answer_covering_us;
+explain select * from demo where answer=42;
+
+show yb_enable_geolocation_costing;
+\q
+
+
+---# END
 
 
 
+-- Security features (RLS, column-level encryption)
+
+-- RLS - example multitenant
+-- CLS instead of view, grant per column
+-- CLE 
+
+create extension if not exists pgcrypto;
+drop table if exists demo;
+create table demo (
+ id uuid default gen_random_uuid() primary key
+ , password text
+);
+
+insert into demo(password) select PGP_SYM_ENCRYPT(format('Welcome%s',generate_series(123,1000)),'xxx');
+select * from demo;
+select id,PGP_SYM_DECRYPT(password::bytea,'xxx') from demo;
+
+
+create index demoi on demo(substr(password,1,10));
+set yb_enable_expression_pushdown = on;
+explain /*+ IndexScan(demo) */ select * from demo where substr(PGP_SYM_ENCRYPT('Welcome123','xxx')::text,1,10)=substr(password,1,10);
+
+select PGP_SYM_ENCRYPT('Welcome123','xxx')::text;
+select password from demo;
+
+
+PGP_SYM_ENCRYPT
+PGP_SYM_DECRYPT -> symetric (need password)
+text or bytea
+
+
+
+create table demo_gdpr ( 
+ user_country char(2),
+  user_uuid uuid default gen_random_uuid(),
+  primary key (user_country, user_uuid)
+) partition by list(user_country);
 
 
 

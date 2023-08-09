e381a434988c64ef58b7e0fb29e7b87db202703f
Franck Pachot <mail@pachot.net>
2023-08-09 09:00:41 +0200

    initialize with 1000 rows

--- docker/grafana/scripts/init/create.sql
+++ docker/grafana/scripts/init/create.sql
@@ -1,5 +1,5 @@
 drop table if exists demo
 create table if not exists demo
 (id bigint, primary key(id asc), num bigint, value text);
-
-\q
+insert into demo ( id, num )
+ select id , 0 from generate_series(1,1000) id;
